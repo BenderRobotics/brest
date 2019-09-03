3590d9085611a0cf582acfde410aa7c36b806caa
Jakub Drajsajtl <drajsajtl@benderrobotics.com>
2019-09-03 09:13:59 +0200

    Added method for test preparing

--- brest/__init__.py
+++ brest/__init__.py
@@ -8,7 +8,14 @@ from .config import Config
 from .resource import Resource
 from .resource_provider import ResourceProvider
 
-__all__ = ['Config', 'Resource', 'Resources', 'ResourceProvider', 'overwrite_log_config']
+__all__ = [
+    'Config',
+    'Resource',
+    'Resources',
+    'ResourceProvider',
+    'overwrite_log_config',
+    'prepare_tests',
+    ]
 
 class Resources():
     '''
@@ -67,6 +74,10 @@ class Resources():
             self.logger.warning('No resources were initialized', extra=self.log_args)
 
 def overwrite_log_config(config_dict):
+    '''
+    Method takes a logging configuration dictionary and merges it with the brest implicit configuration.
+    '''
+
     custom_config = dict(log.DEFAULT_LOGGING)
     for ov_key, ov_value in config_dict.items():
         __apply_overwrite(custom_config, ov_key, ov_value)
@@ -81,3 +92,31 @@ def __apply_overwrite(node, key, value):
                 node[key] = value
     else:
         node[key] = value
+
+def prepare_tests(test_suite, project, config=Config.BREST_CONFIG):
+    '''
+    Method takes test_suite object created using unittest.discover(). It collects
+    all needed resources from test, construct them and sets as class a attribute on
+    every test.
+    Returns Resources instance
+    '''
+
+    # collect needed resources
+    needed = []
+    for folder_suite in test_suite:
+        for file_suite in folder_suite:
+            for test in file_suite:
+                for n in test.needed:
+                    if n not in needed:
+                        needed.append(n)
+
+    # construct them
+    resources = brest.Resources(project, config=config, needed=needed)
+
+    # set constructed resources to every test
+    for folder_suite in test_suite:
+        for file_suite in folder_suite:
+            for test in file_suite:
+                test.resources = resources
+
+    return resources
