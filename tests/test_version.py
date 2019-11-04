68b3d6cf5b5e15984db41742b86abf921a366e3f
Jakub Lesinsky <lesinsky@benderrobotics.com>
2019-11-04 20:34:17 +0100

    Added deploy stage to CI (refs #2532)

---
+++ tests/test_version.py
@@ -0,0 +1,8 @@
+import os
+import brest
+
+curr_ver = brest.__version__
+exp_ver = os.environ['CI_COMMIT_TAG']
+
+msg = 'Version mismatch! (Current={0} != Expected={1})'.format(curr_ver, exp_ver)
+assert curr_ver == exp_ver, msg
